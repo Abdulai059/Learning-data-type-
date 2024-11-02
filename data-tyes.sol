df3af2b8f858cb0603274695aa96723ec9debe42
Abdulai059 <abdulaiosman8080@gmail.com>
2024-11-02 12:44:44 +0000

    learning data types

---
+++ data-tyes.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+contract SimpleStorage {
+    bool hasFavoriteNumber = true;
+    uint256 favoriteNumber = 42;
+    int256 favoriteInt = -42;
+     address owner = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
+    string favoriteNumberInText = "forty-two";
+    bytes32 favoriteBytes = "0x1234567890abcdef1234567890";
+}
